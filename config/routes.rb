a5d22adcc6e4aa3a47f50f2bd1502f31f25ad34e
François Klingler <francois@klingler.fr>
2024-05-27 14:50:30 +0200

    feat(api): add basic Hive resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Scope the API routes so it doesn't conflict with the frontend routes
+  scope '/api' do
+    resources :hives
+  end
+
   # Handle the frontend app
   if Rails.env.development?
     # In development, we can simply redirect to the Vite dev server URL, as this is
