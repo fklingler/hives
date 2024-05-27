0412e5a9ad2c90d7e85f866e64c100d22866dc4c
François Klingler <francois@klingler.fr>
2024-05-27 14:49:23 +0200

    chore: serve public/index.html file properly for all frontend routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Handle the frontend app
+  if Rails.env.development?
+    # In development, we can simply redirect to the Vite dev server URL, as this is
+    # probably what the user wanted
+    get "(*path)", to: redirect("http://localhost:5173/%{path}", status:302)
+  else
+    # In production, serve the public/index.html file for all URLs but assets, as we have
+    # a Single-Page Application with history routing
+    # See https://router.vuejs.org/guide/essentials/history-mode#HTML5-Mode
+    # Snippet adapted from https://stackoverflow.com/a/77184296
+    constraints(path: %r{^(?!/assets)}) do
+      get "(*path)", to: ->(env) {
+        [200, {}, [Rails.root.join("public/index.html").read]]
+      }
+    end
+  end
 end
